0fead3b7054175d503bfeffb200c4d10931ef442
AndreyCG <andreycogo@unisabana.edu.co>
2023-05-27 18:39:00 -0500

    SigaApp-B

---
+++ src/test/java/co/edu/unisabana/SigaApp/SigaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unisabana.SigaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
